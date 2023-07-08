1df3dadc9106687c3a1e6da73b36bc69202d8a8d
devferreira <edilson.mx@gmail.com>
2023-07-07 21:40:43 -0300

    transformação de procedimento em método para ser usuado em outra classe

---
+++ processamentoAuditoria.py
@@ -0,0 +1,26 @@
+import os
+
+
+def lerProcessos(mesCorrente):
+    NpuProcessos = []
+    caminhoPasta = "auditoria"+mesCorrente
+    listaDeArquivos = os.listdir(caminhoPasta)
+
+    for relacao in listaDeArquivos:
+        enderecoPasta = caminhoPasta+'\\'+str(relacao)
+        file = open(enderecoPasta, "r")
+        file.readline()
+        linhasArquivo = file.readlines()
+        for line in linhasArquivo:
+            posicaoMarcador = line.find("-")
+            NpuProcessos.append(
+                line[posicaoMarcador-7:posicaoMarcador+18]+","+line[:posicaoMarcador+18])
+    conjunto = set(NpuProcessos)
+    nome_arquivo_saida = "relacaoAuditoria"+mesCorrente+".csv"
+    arquivoRetorno = open(nome_arquivo_saida, "a")
+
+    for linha in conjunto:
+        print(linha)
+        arquivoRetorno.write(linha+"\n")
+
+lerProcessos("Julho")
