6c0101651f3a0cd127b9013acbaab76cb45294e9
devferreira <edilson.mx@gmail.com>
2023-07-07 21:37:18 -0300

    Revert "projeto de auditoria de processo de digitalização"
    This reverts commit 0e1d57bc9b999076f03a30d12b17b47a45eaa752.

--- CursoHashTag/primeiraAula.py
+++
@@ -1,4 +0,0 @@
-valor_produto = input("qual o valor do produto?")
-qtd_vendas = input("quantidade de produtos vendidos?")
-total = int(valor_produto)*int(qtd_vendas)
-print("O faturamento total foi de "+str(total))
